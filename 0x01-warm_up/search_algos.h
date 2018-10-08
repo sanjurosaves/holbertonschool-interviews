49725a3aca745dce85e6cc939ab2534059f6805d
Alex Allen <325@holbertonschool.com>
2018-10-08 19:48:52 +0000

    add search algos header

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SALGOS_H
+#define _SALGOS_H
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+float min(float x, unsigned int y);
+
+#endif
