b8c1574d665a615447b93b0d402a1b403975eeb3
Alex Allen <325@holbertonschool.com>
2018-10-08 21:47:22 +0000

    update binary full excercise

--- 0x01-warm_up/2-binary_tree_is_full.c
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -1,20 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - checks whether binary tree is full
- * @tree: pointer to head node of tree
- * Return: 1 if tree is full, zero if tree is null or if tree is not full
+ * btf - checks if a binary tree is full
+ * @tree: pointer to tree to check
+ * Return: 1 if full, 0 if not
+ */
+int btf(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if ((tree->left) && (tree->right))
+		return (btf(tree->left) &&
+			btf(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to tree to check
+ * Return: 1 if full, 0 if not
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if ((tree->left == NULL) || (tree->left->left && tree->left->right))
-		if ((tree->right == NULL) || (tree->right->left &&
-					      tree->right->right))
-			return (1);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	return (binary_tree_is_full(tree->left) &&
-	       binary_tree_is_full(tree->right));
+	if ((tree->left) && (tree->right))
+		return (btf(tree->left) &&
+			btf(tree->right));
+
+	return (0);
 }
